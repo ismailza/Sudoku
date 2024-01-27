6f69d5eebaab31d825c8c6f8ad32d1f4d5c4cf69
Ismail ZAHIR <ismailza407@gmail.com>
2024-01-27 21:53:45 +0100

    update

--- test/consoleSolver.cpp
+++ test/consoleSolver.cpp
@@ -7,8 +7,17 @@ using namespace chrono;
 
 int main() {
   high_resolution_clock::time_point start, end;
-  Sudoku s(9);
-  s.initBoard(1);
+  int difficulty, size;
+  do {
+    cout << "Enter difficulty (0 - Easy, 1 - Medium, 2 - Hard): ";
+    cin >> difficulty;
+  } while (difficulty < 0 || difficulty > 2);
+  do {
+    cout << "Enter size (4 - 4x4, 9 - 9x9, 16 - 16x16): ";
+    cin >> size;
+  } while (size != 4 && size != 9 && size != 16);
+
+  Sudoku s(difficulty, size);
   s.printBoard();
   cout << "***********************************" << endl;
   start = high_resolution_clock::now();
